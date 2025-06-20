660e55d70f7cbff442c81de5660b1510a6b1f1bd
Armando Ferlanti <mandynator92@hotmail.com>
2025-06-20 16:42:17 +0200

    Delete PWA directory

--- PWA/sw-register.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('✅ Service Worker registrato:', reg.scope))
-      .catch(err => console.error('❌ Errore nella registrazione del Service Worker:', err));
-  });
-}
